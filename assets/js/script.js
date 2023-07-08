8ffac1c861646f97750136b7a4c57815d177b746
Oliver Lopez <oliverberto@gmail.com>
2023-07-08 15:09:12 -0400

    renamed data to ufc and other dynamic elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
-var fightersList = document.querySelector('ul');
+// We have an initial static element 
+var mmaFights = document.getElementById('mma-fights');
+// Button click to API call
 var fetchButton = document.getElementById('fetch-button');
 
-//getApi function is called when the fetchButton is clicked
+//   getApi function is called when the fetchButton is clicked
 function getApi() {
   // Insert the API url to get a list of your repos
   var requestUrl ='https://api.sportsdata.io/v3/mma/scores/json/Schedule/UFC/2023?key=a9c3522c4f0548e0a9e4e258699591f8';
@@ -10,22 +12,24 @@ function getApi() {
     .then(function (response) {
       return response.json();
     })
-    .then(function (data) {
-        console.log(data);
-      //looping over the fetch response and inserting the URL of your repos into a list
-      for (var i = 0; i < data.length; i++) {
+    .then(function (ufc) {
+        console.log(ufc);
+      //looping over the fetch response and inserting the URL of ufc fights 
+      for (var i = 0; i < ufc.length; i++) {
         //Create elements  
-        var listItem1 = document.createElement('h1');
-        var listItem2 = document.createElement('p');
-        var listItem3 = document.createElement('li');
+        var name = document.createElement('h1');
+        var shortName = document.createElement('p');
+        var day = document.createElement('p');
 
-        //Set the text of the list element to the JSON response's .html_url property
-        listItem1.textContent = data[i].Name;
-        listItem2.textContent = data[i].ShortName;
-        listItem3.textContent = data[i].Day;
+        // Parsing the data
+        name.textContent = ufc[i].Name;
+        shortName.textContent = ufc[i].ShortName;
+        day.textContent = ufc[i].Day;
 
-        //Append the li element to the id associated with the ul element.
-        fightersList.appendChild(listItem1, listItem2, listItem3);
+        // Create dynamic element on the page 
+        mmaFights.append(name);
+        mmaFights.append(shortName);
+        mmaFights.append(day);
       }
     });
 }
